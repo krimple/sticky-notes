af76d73b4be66beadad7f337fb6f4e58501999a5
Ken Rimple <krimple@chariotsolutions.com>
2017-04-29 10:07:55 -0400

    step 1 - say hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
+console.log(`Environment is ${JSON.stringify(environment)}`);
 @Component({
   selector: 'sn-root',
   template: `
-  <h1>
-    {{title}}
-  </h1>
+    <h1>
+      Hello, Angular 4 Students
+    </h1>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'sn works!';
 }
