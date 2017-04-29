e90074c786af5e8bc0124869fb6b79477e912b19
Ken Rimple <krimple@chariotsolutions.com>
2017-04-29 10:25:48 -0400

    added basic note (#2)

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'sn-note',
   template: `
-    <p>
-      I am a sticky note
-    </p>
+    <div class="container">
+      <div class="title">
+        {{ title }}
+      </div>
+      <div class="content">
+        {{ content }}
+      </div>
+    </div>
   `,
-  styles: []
+  styles: [`
+    .container {
+      width: 300px;
+      padding: 10px;
+      border: 1px solid black;
+      background-color: blanchedalmond;
+      color: black;
+    }
+    .title {
+      font-family: sans-serif;
+      font-size: 1.2em;
+      text-align: center;
+      font-weight: bold;
+      border-bottom: 1px solid black;
+    }
+    .content {
+      font-family: sans-serif;
+      font-size: .9em;
+      color: darkslategrey;
+      font-weight: normal;
+    }
+  `]
 })
 export class NoteComponent implements OnInit {
+
+  title = 'I am the title';
+  content = 'This is a note';
 
   constructor() { }
 
